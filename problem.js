227e7e4584f67fecfec6c291a3ac003a835f6559
mdatiqurrahman575 <mdatiqurrahman575@gmail.com>
2026-09-02 13:08:12 +0600

    first commit

---
+++ problem.js
@@ -0,0 +1,84 @@
+function describeValue (value) {
+
+    let type = typeof value;
+
+    if(value){
+        return `${type}| truthy`;
+    } else {
+        return `${type}| falsy`;
+    }
+};
+
+function getDayType(day) {
+    let dayName = day.toLowerCase();
+
+    switch (dayName) {
+        case "friday":
+        case "saturday":
+            return "Weekend";
+
+        case "sunday":
+        case "monday":
+        case "tuesday":
+        case "wednesday":
+        case "thursday":
+            return "Working Day";
+
+        default:
+            return "Invalid Day";
+    }
+};
+
+function validateUsername(username) {
+    if (username.length < 4) {
+        return "Too Short";
+    } else if (username.includes(" ")) {
+        return "No Space Allowed";
+    } else if (username.toLowerCase().includes("admin")) {
+        return "Reserved Word";
+    } else {
+        return "Available";
+    }
+};
+
+function getCngFare(distance, isNight = false, waitingMinutes = 0) {
+    let fare = 50;
+
+    if (distance > 2) {
+        fare = fare + (distance - 2) * 15;
+    }
+
+    fare = fare + waitingMinutes * 2;
+
+    if (isNight) {
+        fare = fare * 1.2;
+    }
+
+    return fare;
+};
+
+const getChaseVerdict = (target, scored, ballsLeft) => {
+    let runsNeeded = target - scored;
+
+    if (runsNeeded <= 0) {
+        return "Won";
+    }
+
+    if (ballsLeft <= 0) {
+        return "Lost";
+    }
+
+    let requiredRate = (runsNeeded / ballsLeft) * 6;
+
+    let verdict;
+
+    if (requiredRate <= 6) {
+        verdict = "Comfortable";
+    } else if (requiredRate <= 12) {
+        verdict = "Tough";
+    } else {
+        verdict = "Almost Impossible";
+    }
+
+    return `Need ${runsNeeded} runs in ${ballsLeft} balls | ${verdict}`;
+};
---
+++ problem1.js
@@ -0,0 +1,25 @@
+const getChaseVerdict = (target, scored, ballsLeft) => {
+    let runsNeeded = target - scored;
+
+    if (runsNeeded <= 0) {
+        return "Won";
+    }
+
+    if (ballsLeft <= 0) {
+        return "Lost";
+    }
+
+    let requiredRate = (runsNeeded / ballsLeft) * 6;
+
+    let verdict;
+
+    if (requiredRate <= 6) {
+        verdict = "Comfortable";
+    } else if (requiredRate <= 12) {
+        verdict = "Tough";
+    } else {
+        verdict = "Almost Impossible";
+    }
+
+    return `Need ${runsNeeded} runs in ${ballsLeft} balls | ${verdict}`;
+};
